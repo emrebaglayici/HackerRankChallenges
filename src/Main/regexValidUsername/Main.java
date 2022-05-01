3a6c340325f5d96847eb69e09d6be2555f4805ca
emre <emrebaglayici1@gmail.com>
2022-05-01 11:26:12 +0300

    HackerRank

---
+++ src/Main/regexValidUsername/Main.java
@@ -0,0 +1,24 @@
+package Main.regexValidUsername;
+
+import java.util.Scanner;
+class UsernameValidator{
+    public static final String regularExpression="^[a-zA-Z][\\w_]{7,29}$";
+
+
+}
+public class Main {
+    private static final Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+            int n = Integer.parseInt(scan.nextLine());
+            while (n-- != 0) {
+                String userName = scan.nextLine();
+
+                if (userName.matches(UsernameValidator.regularExpression)) {
+                    System.out.println("Valid");
+                } else {
+                    System.out.println("Invalid");
+                }
+            }
+
+    }
+}
